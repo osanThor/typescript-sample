635a36a0d2c68ba98ea3b694bd05c10d01c68258
junyoung <jadw9715@gmail.com>
2022-09-28 22:29:29 +0900

    detail 변경경

--- 4-oop-project/stack.ts
+++ 4-oop-project/stack.ts
@@ -1,47 +1,49 @@
-interface Stack {
-  readonly size: number;
-  push(value: string): void;
-  pop(): string;
-}
-type StackNode = {
-  readonly value: string;
-  readonly next?: StackNode;
-};
-
-class StackImpl implements Stack {
-  private _size: number = 0;
-  private head?: StackNode;
-
-  constructor(private capacity: number) {}
-
-  get size() {
-    return this._size;
+{
+  interface Stack {
+    readonly size: number;
+    push(value: string): void;
+    pop(): string;
   }
-  push(value: string) {
-    if (this.size === this.capacity) {
-      throw new Error("Stack is Full");
+  type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode;
+  };
+
+  class StackImpl implements Stack {
+    private _size: number = 0;
+    private head?: StackNode;
+
+    constructor(private capacity: number) {}
+
+    get size() {
+      return this._size;
     }
-    const node: StackNode = { value, next: this.head };
-    this.head = node;
-    this._size++;
-  }
-  pop(): string {
-    if (this.head == null) {
-      throw new Error("Stack is empty");
+    push(value: string) {
+      if (this.size === this.capacity) {
+        throw new Error("Stack is Full");
+      }
+      const node: StackNode = { value, next: this.head };
+      this.head = node;
+      this._size++;
     }
+    pop(): string {
+      if (this.head == null) {
+        throw new Error("Stack is empty");
+      }
 
-    const node = this.head;
-    this.head = node.next;
-    this._size--;
-    return node.value;
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
+    }
   }
-}
 
-const stack = new StackImpl(10);
-stack.push("Ellie 1");
-stack.push("Bob 2");
-stack.push("Steve 3");
-while (stack.size !== 0) {
-  console.log(stack.pop());
+  const stack = new StackImpl(10);
+  stack.push("Ellie 1");
+  stack.push("Bob 2");
+  stack.push("Steve 3");
+  while (stack.size !== 0) {
+    console.log(stack.pop());
+  }
+  stack.pop();
 }
-stack.pop();
